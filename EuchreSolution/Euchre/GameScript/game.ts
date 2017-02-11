565ee1e330a5a70f90b080ba34e5bac22bc6ae6e
Kevin Lyles <kevinlyles@gmail.com>
2017-02-11 12:59:30 -0600

    Stuff

--- EuchreSolution/Euchre/GameScript/game.ts
+++ EuchreSolution/Euchre/GameScript/game.ts
@@ -249,9 +249,9 @@ class Game {
 		//else this.__messageLevel = (document.getElementById("chkStatMode") as HTMLInputElement).checked;
 		this.__aiPlayers = [
 			new MultiAI(new KevinAI(), new DecentAI()),
-			new MultiAI(new DecentAI(), new KevinAI()),
+			new KevinAI(),
 			new MultiAI(new KevinAI(), new DecentAI()),
-			new MultiAI(new DecentAI(), new KevinAI()),
+			new KevinAI(),
 		];
 		this.__hasHooman = this.__aiPlayers.indexOf(null) > -1;
 	}
