288239c8778eacffcd02a028ca0a3edcd778781b
Kevin Lyles <kevinlyles@gmail.com>
2018-01-01 16:31:46 -0600

    Make animController compile

--- EuchreSolution/Euchre/GameScript/animController.ts
+++ EuchreSolution/Euchre/GameScript/animController.ts
@@ -5,55 +5,34 @@ enum AnimType {
 	WinTrick,
 }
 
-class Animation {
-	public readonly animType: AnimType;
-	public readonly player: Player | null;
-	public readonly cardID: string | null;
-	public readonly text: string | null;
-
-	constructor(animType: AnimType, player?: Player, cardID?: string, text?: string) {
-		this.animType = animType;
-		this.player = player === undefined ? null : player;
-		this.cardID = cardID === undefined ? null : cardID;
-		this.text = text === undefined ? null : text;
-	}
-
+interface Animation {
+	readonly animType: AnimType;
+	readonly delay: number;
+	readonly callback: () => void;
+	readonly player?: Player;
+	readonly cardID?: string;
+	readonly text?: string;
 }
+
+const delays = {
+	[AnimType.DealHands]: 200,
+	[AnimType.PlayCard]: 500,
+	[AnimType.Discard]: 500,
+	[AnimType.WinTrick]: 500,
+};
 
 class AnimController {
 	public queuedAnimations: Animation[];
 
-	public pushAnimation(animType: AnimType, player?: Player, cardID?: string, text?: string): void {
-		let animation: Animation = new Animation(animType, player, cardID, text);
+	public pushAnimation(animType: AnimType, callback: () => void, player?: Player, cardID?: string, text?: string): void {
+		const animation: Animation = { animType, delay: delays[animType], callback, player, cardID, text };
 		this.queuedAnimations.push(animation);
 	}
 
 	public executeAnimations(): void {
-		let pauseTime: number = 500;
-		let delay: number;
-		let animFunction: Function;
-
 		if (this.queuedAnimations.length <= 0) { return; }
 
-		for (let i = 0; i < this.queuedAnimations.length; i++) {
-			delay = pauseTime * i;
-			animFunction = this.getAnimFunction(animType, player, cardID);
-
-		}
-	}
-
-	public getAnimFunction(animType: AnimType, player?: Player, cardID?: string): Function {
-		switch (animType) {
-			case AnimType.DealHands:
-				return animDeal();
-			case AnimType.Discard:
-				break;
-			case AnimType.PlayCard;
-				break;
-			case AnimType.WinTrick;
-				break;
-			default:
-				break;
-		}
+		const animation = this.queuedAnimations.shift() as Animation;
+		setTimeout(animation.callback, animation.delay, animation.player, animation.cardID, animation.text);
 	}
 }
